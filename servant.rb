5ddf1f6b7793d07ceece8c8f99447f37272d5b85
mtomczak <iam@fixermark.com>
2011-01-29 18:07:52 -0500

    Initial commit.

---
+++ servant.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'belphanior/servant/servant'
+require 'sinatra'
+
+# Must be called to install and configure default routing for
+# '/', '/config', and '/protocol'
+servant_init
