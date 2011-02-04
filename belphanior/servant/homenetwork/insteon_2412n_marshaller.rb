bb43d6b92040aaa31b4e708db8ca6b3bd3862b53
mtomczak <iam@fixermark.com>
2011-02-04 08:07:01 -0500

    Added marshaller and test for same.

---
+++ belphanior/servant/homenetwork/insteon_2412n_marshaller.rb
@@ -0,0 +1,29 @@
+# Marshaller for the Insteon 2412N home automation server
+require 'net/http'
+
+module Belphanior
+  module Servant
+    module HomeNetwork
+      class Insteon_2412n_Marshaller
+        class ConnectionFailure < RuntimeError
+        end
+        def initialize(host, network_class)
+          @host = host
+          if network_class
+            @network_class = network_class
+          else
+            @network_class = Net::HTTP
+          end
+        end
+        def send(command)
+          @network_class.start(@host, 80) {|http|
+            # I don't know what the prefix and suffix here mean;
+            # they were determined empirically by examining the output
+            # from the 2412N's built-in web GUI
+            http.post("/3?"+command+"=I=3", "")
+          }
+        end
+      end
+    end
+  end
+end
---
+++ test/tc_insteon_2412n_marshaller.rb
@@ -0,0 +1,20 @@
+require 'belphanior/servant/homenetwork/insteon_2412n_marshaller'
+require 'test/unit'
+require 'mocha'
+
+
+class TestInsteon_2412N_Marshaller < Test::Unit::TestCase
+  def setup
+    @netmock = mock('Net::HTTP')
+    @requestmock = mock()
+    @marshaller = 
+      Belphanior::Servant::HomeNetwork::Insteon_2412n_Marshaller.new(
+      'localhost', @netmock)
+  end
+  def test_send
+    input = "02636600"
+    @netmock.expects(:start).with('localhost',80).yields(@requestmock)
+    @requestmock.expects(:post).with("/3?"+input+"=I=3","")
+    @marshaller.send(input)
+  end
+end
